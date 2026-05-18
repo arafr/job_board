363c606dd4d14bb98538e9d6e10baf68edfe7f6a
Araf Rahman <arafrahman563@gmail.com>
2026-05-18 21:23:41 +1000

    added test file

---
+++ test_app.py
@@ -0,0 +1,20 @@
+# UNIT TESTS
+
+# test signup employer
+# test login employer
+
+# test signup job seeker
+# test login job seeker
+
+# test if skills are loaded into database
+
+# test new job creation
+
+# test app load time
+
+# INTEGRATION TESTS
+
+# test authentication for employer (only logged in employers can view job creation and talent board pages)
+
+# test authentication for seeker (only logged in seekers can view job board page)
+
