f55f8de15a96d736cc8f619421ae60685e6871ee
Araf Rahman <arafrahman563@gmail.com>
2026-05-28 15:23:12 +1000

    integrated into main app file

--- fuzz.py
+++
@@ -1,7 +0,0 @@
-from rapidfuzz import fuzz
-
-job = Job.query.filter(Job.title.ilike("%software engineer%")).first()
-search_match_ratio = fuzz.WRatio("software engineer", job.title)
-
-if search_match_ratio > 70:
-    return job
